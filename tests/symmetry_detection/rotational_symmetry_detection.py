78cfd3c85093c8c274f666f76362291c9e2b0ab5
simulatedScience <62523381+simulatedScience@users.noreply.github.com>
2024-05-30 10:38:36 +0200

    implemented rotation pruning based on quarternion distance

--- tests/symmetry_detection/rotational_symmetry_detection.py
+++ tests/symmetry_detection/rotational_symmetry_detection.py
@@ -1,12 +1,13 @@
 
 """
-This moidule implements rotational symmetry detection for a 3D point cloud as described in (Hruda et. al.) [https://doi.org/10.1016/j.cagd.2022.102138].
+This moidule implements rotational symmetry detection for a 3D point cloud as described in [Hruda et. al.](https://doi.org/10.1016/j.cagd.2022.102138) [1].
 author: Sebastian Jost
 """
 
 import numpy as np
 from scipy.optimize import minimize
-from scipy.spatial.transform import Rotation
+from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Slerp
 
 from symmetry_plane_detection import find_symmetry_planes, dist_similarity_function
 
@@ -24,7 +25,7 @@ def find_plane_intersection(
         tol (float, optional): tolerance for detecting parallel planes. Defaults to 1e-15.
 
     Returns:
-        tuple[np.ndarray, np.ndarray]: intersection line defined by a point and a direction vector
+        tuple[np.ndarray, np.ndarray]: intersection line defined by a point and a normalized direction vector
     """
     # Extract plane coefficients
     a1, b1, c1, d1 = plane_1
@@ -81,7 +82,7 @@ def rotation_symmetry_measure(X: np.ndarray, rotation: tuple[float, np.ndarray],
     Returns:
         float: symmetry measure in range [0, inf) (see (Hruda et. al.) Eq. (3))
     """
-    
+    # TODO: Optimize by exploiting the locality of the symmetry measure (use cell-list or similar data structure)
     n: int = X.shape[0]  # number of points
     transformed_X = rotate_points(X, rotation[0], rotation[1])
     similarity_measure = 0
@@ -101,6 +102,9 @@ def find_rotational_symmetries(
         min_angle: float = np.pi / 12.5,
         num_best_rotations: int = 30,
         alpha: float = 1.0,
+        epsilon_Q: float = 0.05,
+        epsilon_s: float = 0.05, # should be 0.05 l_avg
+        min_score_ratio: float = 0.7,
     ) -> list[tuple[np.ndarray, float]]:
     """
     Detect rotational symmetries in a 3D point cloud.
@@ -108,47 +112,85 @@ def find_rotational_symmetries(
     Args:
         X (np.ndarray): set of points
         num_planes (int): number of planes to generate
+        num_candidate_rotations (int): number of candidate rotations to generate
         threshold (float): threshold to check if a plane is too similar to existing planes
-
+        min_angle (float): minimum angle between two planes to consider them for rotation detection in radians (Default: np.pi / 12.5 = just under 15°)
+        num_best_rotations (int): number of best rotations to keep
+        alpha (float): parameter to control the similarity function
+        epsilon_Q (float): tolerance for quaternion similarity
+        epsilon_s (float): tolerance for axis support similarity
+        min_score_ratio (float): minimum score relative to the best score to keep a rotation
     Returns:
         list[tuple[np.ndarray, float]]: list of rotational symmetries (axis, angle) detected
     """
     # Step 1: Find reflectional symmetry planes
     planes = find_symmetry_planes(X, num_planes, threshold, S=num_candidate_rotations)
 
-    # Step 2: Find intersections of the planes
-    best_rotations = []
-    best_scores = []
+    # Step 2: Find rotation candidates
+    #   2.1 find intersections of pairs of planes
+    #   2.2 prune candidates that are too similar to existing ones
+    pruned_candidates = []
     for _ in range(num_planes//10):
+        # Step 2.1: Select two planes and find their intersection
         idx_1: int = np.random.randint(len(planes) - 1)
         idx_2: int = np.random.randint(idx_1 + 1, len(planes))
         plane_1 = planes[idx_1]
         plane_2 = planes[idx_2]
         # print(f"{np.dot(plane_1[1], plane_2[1]) = }")
-        intersection_angle: float = np.arccos(np.dot(plane_1[:3], plane_2[:3]))  # angle between normals
+        rotation_angle: float = 2*np.arccos(np.dot(plane_1[:3], plane_2[:3]))  # angle of rotation = 2*angle between normals
         # reject planes that are too similar (small rotation angle)
-        if intersection_angle < min_angle/2:
+        if rotation_angle < min_angle:
             continue
         intersection = find_plane_intersection(plane_1, plane_2)
         if intersection is None:
             continue # planes are parallel (should never happen here)
         axis_support, axis = intersection
-        rotation_angle: float = 2*intersection_angle
         # make sure rotation_angle is in [-pi, pi]
         rotation_angle = rotation_angle % (2*np.pi)
         if rotation_angle > np.pi:
             rotation_angle -= 2*np.pi
-        # translate X by line_support
-        X_translated: np.ndarray = X - axis_support
+        # Step 2.2: Prune rotation candidates if they are too similar to existing ones
+        quaternion = R.from_rotvec(rotation_angle * axis).as_quat()
+        # candidate: (quaternion, axis_support) = (Q,s) in paper [1]
+        for i, (Q_i, s_i, w_i) in enumerate(pruned_candidates):
+            if (np.linalg.norm(quaternion - Q_i * np.sign(np.dot(quaternion, Q_i))) < epsilon_Q and
+                    np.linalg.norm(axis_support - s_i) < epsilon_s):
+                # Merge the candidates
+                new_weight = w_i + 1 # w(quarternion, axis_support) := 1
+                s_new = (w_i * s_i + axis_support) / new_weight
+                
+                # Use Slerp for quaternion interpolation
+                key_rots = R.from_quat([Q_i, quaternion])
+                slerp = Slerp([0, 1], key_rots)
+                Q_new = slerp(1 / new_weight).as_quat()
+                pruned_candidates[i] = (Q_new, s_new, new_weight)
+                break
+        else:
+            pruned_candidates.append((quaternion, axis_support, 1)) # (Q, s, w(Q, s))
+
+    # Step 3: Evaluate candidate rotations and find best ones
+    best_rotations = []
+    best_scores = []
+    for Q, s, _ in pruned_candidates: # weights (w(Q, s)) are no longer needed
+        axis = R.from_quat(Q).as_rotvec()
+        rotation_angle = np.linalg.norm(axis)
+        axis /= rotation_angle
+        X_translated = X - s
         score = rotation_symmetry_measure(X_translated, rotation=(rotation_angle, axis), alpha=alpha)
         if len(best_scores) < num_best_rotations or score > min(best_scores):
             if len(best_scores) >= num_best_rotations:
                 min_index = np.argmin(best_scores)
                 best_scores[min_index] = score
-                best_rotations[min_index] = (rotation_angle, axis, axis_support)
+                best_rotations[min_index] = (rotation_angle, axis, s)
             else:
                 best_scores.append(score)
-                best_rotations.append((rotation_angle, axis, axis_support))
+                best_rotations.append((rotation_angle, axis, s))
+
+    # Step 4: remove rotations with score < best_score * min_score_ratio
+    best_score = max(best_scores)
+    best_rotations = [rotation for rotation, score in zip(best_rotations, best_scores) if score >= best_score * min_score_ratio]
+    del best_scores # best_scores no longer accurate since best_rotations has been pruned
+
     def objective(rotation_components):
         angle: float = rotation_components[0]
         axis: np.ndarray = rotation_components[1:4]
@@ -200,5 +242,5 @@ def rotate_points(X: np.ndarray, angle: float, axis: np.ndarray, tol: float = 1e
     if norm - 1 > tol:
         # normalize axis
         axis = axis / norm
-    rotation = Rotation.from_rotvec(angle * axis)
+    rotation = R.from_rotvec(angle * axis)
     return rotation.apply(X)
