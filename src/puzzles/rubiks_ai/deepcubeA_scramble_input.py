ce45e842642530e4f09433793b46a3b91d30d7fd
simulatedScience <62523381+simulatedScience@users.noreply.github.com>
2024-10-24 17:08:07 +0200

    [feat] added script to automatically input scrambles into deepcubeA website by simulating keypresses

---
+++ src/puzzles/rubiks_ai/deepcubeA_scramble_input.py
@@ -0,0 +1,73 @@
+"""
+This file implements tools to input scramble sequences into the deepcubeA solver website (https://deepcube.igb.uci.edu/) by simulating keyboard inputs.
+"""
+import time
+
+from pynput.keyboard import Key, Controller
+
+KEYBOARD = Controller()
+
+def press_key(key, shift_modifier: bool = False):
+    """
+    Presses a key on the keyboard.
+    """
+    if shift_modifier:
+        with KEYBOARD.pressed(Key.shift):
+            KEYBOARD.press(key)
+            KEYBOARD.release(key)
+    else:
+        KEYBOARD.press(key)
+        KEYBOARD.release(key)
+
+def print_countdown(length: int = 10):
+    for i in range(length, 0, -1):
+        print(f"\rStarting in {i} seconds...", end="")
+        time.sleep(1)
+    print("Starting! The programm will automatically press keys now.")
+
+def enter_move_sequence(
+        move_sequence: str,
+        start_delay: float = 10,
+        move_delay: float = 0.2):
+    """
+    Convert a given move sequence string into a sequence of keypresses.
+    
+    Args:
+        move_sequence (str): The move sequence to input. moves are separated by spaces. Valid moves are:
+        F, R, U, B, L, D and their inverses F', R', U', B', L', D'.
+        These get converted into key presses
+    """
+    print_countdown(start_delay)
+    for move in move_sequence.split():
+        key, shift_modifier = convert_move(move)
+        press_key(key, shift_modifier)
+        time.sleep(move_delay)
+
+def convert_move(move: str) -> tuple[str, str]:
+    """
+    Converts a move string into a key press instruction.
+    
+    Args:
+        move (str): A single move to convert.
+    
+    Returns:
+        tuple[str, str]: The key press instruction, consisting of a key and a modifier. This can be passed directly to press_key:
+    """
+    if "'" in move:
+        return move[:-1], True
+    return move.lower(), False
+    modifier = "shift" if "'" in move else None
+    move = move.replace("'", "")
+    move = move.lower()
+    return move, modifier
+
+def main():
+    move_sequence: str = "B D U F F L' F' D R D' L B' D B' D R' D' L R' L' L' D U R' D F' U' R L F'"
+    # move_sequence = "F F' U U' R R' D D' L L' B B'"
+    enter_move_sequence(
+        move_sequence,
+        start_delay=5,
+        move_delay=.2)
+    
+if __name__ == "__main__":
+    main()
